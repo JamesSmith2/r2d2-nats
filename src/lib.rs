54f4f82bfc1cffe563c2861cb5f3356a7cc82e8b
Jaanus <jaanus@lepik.net>
2021-01-11 06:46:34 +0200

    upgrade code to match 0.8 ver

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,7 @@
 pub extern crate nats;
 pub extern crate r2d2;
 
-use nats::{Client, NatsError};
-use std::error;
-use std::error::Error as _StdError;
-use std::fmt;
-
-#[derive(Debug)]
-pub enum Error {
-    Other(NatsError),
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        match self.cause() {
-            Some(cause) => write!(fmt, "{}: {}", self.description(), cause),
-            None => write!(fmt, "{}", self.description()),
-        }
-    }
-}
-
-impl error::Error for Error {
-    fn description(&self) -> &str {
-        match *self {
-            Error::Other(ref err) => err.description(),
-        }
-    }
-    fn cause(&self) -> Option<&error::Error> {
-        match *self {
-            Error::Other(ref err) => err.cause(),
-        }
-    }
-}
+use std::io::Error;
 
 #[derive(Debug)]
 pub struct NatsConnectionManager {
@@ -39,7 +9,7 @@ pub struct NatsConnectionManager {
 }
 
 impl NatsConnectionManager {
-    pub fn new(connection_string: String) -> Result<NatsConnectionManager, NatsError> {
+    pub fn new(connection_string: String) -> Result<NatsConnectionManager, Error> {
         Ok(NatsConnectionManager {
             params: connection_string,
         })
@@ -47,24 +17,25 @@ impl NatsConnectionManager {
 }
 
 impl r2d2::ManageConnection for NatsConnectionManager {
-    type Connection = Client;
+    type Connection = nats::Connection;
     type Error = Error;
 
-    fn connect(&self) -> Result<Client, Error> {
-        match Client::new(self.params.to_owned()) {
+    fn connect(&self) -> Result<nats::Connection, Error> {
+        nats::connect(&self.params.to_owned())
+        /*match Client::new(self.params.to_owned()) {
             Ok(client) => Ok(client),
             Err(err) => Err(Error::Other(err)),
-        }
+        }*/
     }
 
-    fn is_valid(&self, conn: &mut Client) -> Result<(), Error> {
+    fn is_valid(&self, conn: &mut nats::Connection) -> Result<(), Error> {
         match conn.publish("r2d2_nats", "PING".as_bytes()) {
             Ok(_) => Ok(()),
-            Err(err) => Err(Error::Other(err)),
+            Err(err) => Err(err),
         }
     }
 
-    fn has_broken(&self, _conn: &mut Client) -> bool {
+    fn has_broken(&self, _conn: &mut nats::Connection) -> bool {
         false
     }
 }
